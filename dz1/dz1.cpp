9810ed0953fe544cb41d4db6d624046c73a084be
nikit <nikit@NIKILAIK>
2023-10-01 14:09:55 +0300

    Добавьте файлы проекта.

---
+++ dz1/dz1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+using namespace std;
+
+int main() {
+
+    while (true) {
+        SetConsoleCP(1251);
+        SetConsoleOutputCP(1251);
+        string user_input;
+        cin >> user_input;
+        if (user_input == "привет") {
+            cout << "Привет!" << endl;
+        }
+        else if (user_input == "пока") {
+            cout << "Пока!"; 
+            break;
+        }
+        else {
+            cout << "Я не совсем понимаю вас." << endl;
+        }
+    }
+
+    return 0;
+}
